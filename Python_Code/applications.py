fd6a1a1de6469916f28fa1bf9a801d93edb51417
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-08-17 01:56:47 -0600

    Small changes

--- Python_Code/applications.py
+++ Python_Code/applications.py
@@ -24,11 +24,14 @@ def print_EV(HAM: Matrix, UMT: Matrix) -> None:
 def print_diag_mtx_info(HAM: Matrix, UMT: Matrix) -> None:
   """ Print Hamiltonian, Eigenvalues and Eigenvectors and 2s wavefunction in terms of eigenstates """
   NBS = len(HAM)
+
   print("Update HAM")
   print_mtx(HAM)
+
   #Prove that the inverse of UMT is the transpose of UMT
   print_EV(HAM, UMT)
   OVR = UMT.transpose()
+
   print("2s Wavefunction in terms of new eigenstates")
   print(str(["{0:10.7f}".format(OVR[1,j]) for j in range(len(OVR[0]))]).replace("'",""))
   pass
@@ -37,8 +40,8 @@ def calc_something(HAM: Matrix, UMT: Matrix, DIP: Matrix, t: float) -> None:
   NBS = len(HAM)
   for k in range(NBS):
     for j in range(NBS):
-      ar = sum([COSINE(HAM[i,i]*t)*UMT[k,i]*UMT[j,i]] for i in range(NBS))
-      ai = sum([SINE(HAM[i,i]*t)*UMT[k,i]*UMT[j,i]] for i in range(NBS))
+      ar = sum([COSINE(HAM[i,i]*t)*UMT[k,i]*UMT[j,i] for i in range(NBS)])
+      ai = sum([SINE(HAM[i,i]*t)*UMT[k,i]*UMT[j,i] for i in range(NBS)])
       PRD[k,j] = ar*ar + ai*ai
 
 # |phi_2t> = Sum_i u(2,i)exp(i eps_i t) |psi_i>
@@ -49,6 +52,7 @@ def calc_something(HAM: Matrix, UMT: Matrix, DIP: Matrix, t: float) -> None:
       phs = (HAM[i,i] - HAM[j,j])*t
       dr += COSINE(phs)*UMT(1,i)*UMT(1,j)*DIP[i,j]
       di += SINE(phs)*UMT(1,i)*UMT(1,j)*DIP[i,j]
+
   print('Finish printing information')
   pass
 
